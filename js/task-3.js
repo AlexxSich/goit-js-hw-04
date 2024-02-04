5c1ebfd084d4b39c5932e6ea24fe3205bd3519b8
AlexxSich <147013014+AlexxSich@users.noreply.github.com>
2024-02-04 10:22:20 -0400

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+console.log("task #3")
+
+function filterArray(numbers, value) {
+    const biggerNum = [];
+
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > value) { 
+            biggerNum.push(numbers[i]);
+        }
+    } 
+    return biggerNum;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
